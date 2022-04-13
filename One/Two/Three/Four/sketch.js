e28241189ccef245ceb1087242354c5c481f1ead
AvadhPatel123 <97483351+AvadhPatel123@users.noreply.github.com>
2022-04-13 08:10:44 -0500

    Update sketch.js

--- One/Two/Three/Four/sketch.js
+++ One/Two/Three/Four/sketch.js
@@ -11,14 +11,14 @@ function setup() {
   button.size(100, 50);
   
   button2 = createButton('Public Transport');
-  button2.position(windowWidth/4 * 3, windowHeight/4);
+  button2.position(windowWidth/4 * 3 - 100, windowHeight/4);
   button2.mousePressed(changeBG2);
   button2.size(100, 50);
   
-  loadImage('transport.jepg', img => {
-    image(img, windowWidth/2, windowHeight/4 * 3, 300, 300);
+  loadImage('transport.jpeg', img => {
+    image(img, windowWidth/2 - 140, windowHeight/4 * 3 - 200, 300, 300);
   });
-
+}
 
 function changeBG() {
   let wrong = color(255, 0, 0);
